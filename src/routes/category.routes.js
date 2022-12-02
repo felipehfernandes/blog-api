844c346d5ed637bd91fe294cf36a0fe9624cddd7
felipehfernandes <felipehfernandes1807@gmail.com>
2022-12-01 21:39:24 -0300

    req 08: creating endpoint POST /categories

---
+++ src/routes/category.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const categoryController = require('../controllers/category.controller');
+const categoryMiddleware = require('../middlewares/categoryName.middleware');
+const authenticationMiddleware = require('../middlewares/authentication.middleware');
+
+const router = express.Router();
+
+router.post('/', authenticationMiddleware, categoryMiddleware, categoryController.createCategory);
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const loginRouter = require('./login.routes');
 const userRouter = require('./user.routes');
+const categoryRouter = require('./category.routes');
 
 const router = express.Router();
 
 router.use('/login', loginRouter);
 router.use('/user', userRouter);
+router.use('/categories', categoryRouter);
 
 module.exports = router;
